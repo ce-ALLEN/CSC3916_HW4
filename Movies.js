b4b9b88211294b4eff7ddcfdc0d8173b7210ef67
Christopher Allen <93797239+ce-ALLEN@users.noreply.github.com>
2022-04-23 14:04:35 -0600

    Movies.js --> added img URL as field

--- Movies.js
+++ Movies.js
@@ -17,7 +17,8 @@ let MovieSchema = new Schema({
     title: {type: String, required: true, index: { unique: true}},
     yearReleased: {type: String, required: true},
     genre: {type: String, required: true},
-    actors: [{actorName: String, characterName: String}]
+    actors: [{actorName: String, characterName: String}],
+    img: {data: Buffer, contentType: String}
 });
 
 
